a140b5da44cb60ac2e615f4211c4f4983e3df348
joe-corcoran <joecorcoran.com@gmail.com>
2024-09-18 18:09:10 -0400

    Phase 3 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,30 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-//const apiRouter = require('./api');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-//router.use('/api', apiRouter);
+// Connect restoreUser middleware to the API router
+router.use(restoreUser);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
   });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
 
+// GET /api/restore-user
+router.get('/restore-user', (req, res) => {
+  return res.json(req.user);
+});
+
+// GET /api/require-auth
+router.get('/require-auth', requireAuth, (req, res) => {
+  return res.json(req.user);
+});
 
 module.exports = router;
