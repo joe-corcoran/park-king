fb160498350aaf497bbffbe7d4a3db26f8e788f8
Joe Corcoran <joecorcoran.com@gmail.com>
2024-09-23 17:32:17 -0400

    delete booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -189,6 +189,39 @@ router.put('/:bookingId', requireAuth, async (req, res, next) => {
       next(error);
     }
   });
+
+  // Delete a Booking
+router.delete('/:bookingId', requireAuth, async (req, res, next) => {
+    const { bookingId } = req.params;
+    const userId = req.user.id;
+  
+    try {
+      const booking = await Booking.findByPk(bookingId, {
+        include: [{ model: Spot }],
+      });
+  
+      if (!booking) {
+        return res.status(404).json({ message: "Booking couldn't be found" });
+      }
+  
+      if (booking.userId !== userId && booking.Spot.ownerId !== userId) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+  
+      const currentDate = new Date();
+      if (new Date(booking.startDate) <= currentDate) {
+        return res.status(403).json({
+          message: "Bookings that have been started can't be deleted",
+        });
+      }
+  
+      await booking.destroy();
+      res.json({ message: "Successfully deleted" });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
   
 
 module.exports = router;
