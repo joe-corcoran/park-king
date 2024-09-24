3caed085dfa2eddb4211fdcbb6393de70f00a979
heather-yaple <heatherlyaple@gmail.com>
2024-09-24 10:51:36 -0400

    Merge Conflict

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,12 +7,93 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-<<<<<<< HEAD
 
-=======
       User.hasMany(models.Review, { foreignKey: 'userId' });
       User.hasMany(models.Booking, { foreignKey: 'userId' });
->>>>>>> bcd5b1dc63822f46aad951bb2d7753dc73b17805
+    }
+  }
+
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 50], 
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 50], 
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error('Cannot be an email.');
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+
+      defaultScope: {
+        attributes: {
+          include: ['id', 'firstName', 'lastName', 'username'],
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ['hashedPassword'] },
+        },
+        withEmail: {
+          attributes: { exclude: ['hashedPassword'] },
+        },
+      },
+    }
+  );
+  return User;
+};
+
+'use strict';
+
+const { Model, Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+
+=========
+      User.hasMany(models.Review, { foreignKey: 'userId' });
+      User.hasMany(models.Booking, { foreignKey: 'userId' });
+>>>>>>>>> Temporary merge branch 2
     }
   }
 
