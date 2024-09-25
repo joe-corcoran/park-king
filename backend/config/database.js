ee7169c7cb8e796bf92a2d1bb187aef0dff55a55
Joe Corcoran <joecorcoran.com@gmail.com>
2024-09-25 12:07:46 -0400

    migrate production database

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,18 +8,60 @@ module.exports = {
     logQueryParameters: true,
     typeValidation: true
   },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    define: {
-      schema: process.env.SCHEMA
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     },
+//     define: {
+//       schema: process.env.SCHEMA
+//     }
+//   }
+// };
+
+production: {
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  seederStorage: 'sequelize',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+  define: {
+    schema: process.env.SCHEMA
   }
+},
+production_url: {
+  dialect: 'postgres',
+  seederStorage: 'sequelize',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  define: {
+    schema: process.env.SCHEMA
+  }
+},
+production_url_ssl: {
+  dialect: 'postgres',
+  seederStorage: 'sequelize',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  define: {
+    schema: process.env.SCHEMA
+  }
+}
 };
