656008fa4e13fd1c0b2d6d9a9f0b63e770783255
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-14 16:16:23 -0400

    Sign up new user complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -76,6 +76,25 @@ const removeUser = () => {
   };
 };
 
+//sign-up new user
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  //login existing user
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
