44ef796ec1a6e8b3907e42baca41b4239a4d56e4
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-15 16:47:11 -0400

    Prepare for deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,24 @@
  //backend/routes/index.js
  
-const express = require('express');
-const router = express.Router();
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-
-module.exports = router;
+ const express = require('express');
+ const path = require('path');
+ const router = express.Router();
+ const apiRouter = require('./api');
+ 
+ router.use('/api', apiRouter);
+ 
+ if (process.env.NODE_ENV === 'production') {
+   router.get('/', (req, res) => {
+     res.cookie('XSRF-TOKEN', req.csrfToken());
+     res.sendFile(path.resolve(__dirname, '../../frontend/dist', 'index.html'));
+   });
+ 
+   router.use(express.static(path.resolve(__dirname, '../../frontend/dist')));
+ 
+   router.get(/^(?!\/?api).*/, (req, res) => {
+     res.cookie('XSRF-TOKEN', req.csrfToken());
+     res.sendFile(path.resolve(__dirname, '../../frontend/dist', 'index.html'));
+   });
+ }
+ 
+ module.exports = router;
