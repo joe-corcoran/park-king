432e6412de1b97899a902ba6d41e1dd570074a02
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-18 22:16:27 -0400

    move sequelize to dependencies for deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,72 @@
+// const config = require('./index');
+
+// module.exports = {
+//   development: {
+//     storage: config.dbFile,
+//     dialect: "sqlite",
+//     seederStorage: "sequelize",
+//     logQueryParameters: true,
+//     typeValidation: true
+//   },
+// //   production: {
+// //     use_env_variable: 'DATABASE_URL',
+// //     dialect: 'postgres',
+// //     seederStorage: 'sequelize',
+// //     dialectOptions: {
+// //       ssl: {
+// //         require: true,
+// //         rejectUnauthorized: false
+// //       }
+// //     },
+// //     define: {
+// //       schema: process.env.SCHEMA
+// //     }
+// //   }
+// // };
+
+// production: {
+//   use_env_variable: 'DATABASE_URL',
+//   dialect: 'postgres',
+//   seederStorage: 'sequelize',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   define: {
+//     schema: process.env.SCHEMA
+//   }
+// },
+// production_url: {
+//   dialect: 'postgres',
+//   seederStorage: 'sequelize',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   define: {
+//     schema: process.env.SCHEMA
+//   }
+// },
+// production_url_ssl: {
+//   dialect: 'postgres',
+//   seederStorage: 'sequelize',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   define: {
+//     schema: process.env.SCHEMA
+//   }
+// }
+// };
+
+
 const config = require('./index');
 
 module.exports = {
@@ -8,60 +77,18 @@ module.exports = {
     logQueryParameters: true,
     typeValidation: true
   },
-//   production: {
-//     use_env_variable: 'DATABASE_URL',
-//     dialect: 'postgres',
-//     seederStorage: 'sequelize',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     },
-//     define: {
-//       schema: process.env.SCHEMA
-//     }
-//   }
-// };
-
-production: {
-  use_env_variable: 'DATABASE_URL',
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
     }
-  },
-  define: {
-    schema: process.env.SCHEMA
   }
-},
-production_url: {
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  define: {
-    schema: process.env.SCHEMA
-  }
-},
-production_url_ssl: {
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  define: {
-    schema: process.env.SCHEMA
-  }
-}
 };
