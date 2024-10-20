4bb0cadd9d9045de64a4c6c74a383a4ef0d9e534
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-20 18:39:45 -0400

    error handlign sign up

--- frontend/src/components/SignupFormPage/SignupFormModal.jsx
+++ frontend/src/components/SignupFormPage/SignupFormModal.jsx
@@ -33,7 +33,16 @@ function SignupFormModal() {
       .then(closeModal)
       .catch(async (res) => {
         const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
+        if (data && data.errors) {
+          const updatedErrors = {};
+          if (data.errors.email) {
+            updatedErrors.email = "The provided email is invalid.";
+          }
+          if (data.errors.username) {
+            updatedErrors.username = "Username must be unique.";
+          }
+          setErrors(updatedErrors);
+        }
       });
   };
 
@@ -48,28 +57,6 @@ function SignupFormModal() {
       </div>
 
       <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      {errors.email && <p className="error-message">{errors.email}</p>}
-
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      {errors.username && <p className="error-message">{errors.username}</p>}
-
-      <label>
         First Name
         <input
           type="text"
@@ -78,7 +65,6 @@ function SignupFormModal() {
           required
         />
       </label>
-      {errors.firstName && <p className="error-message">{errors.firstName}</p>}
 
       <label>
         Last Name
@@ -89,7 +75,26 @@ function SignupFormModal() {
           required
         />
       </label>
-      {errors.lastName && <p className="error-message">{errors.lastName}</p>}
+
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
 
       <label>
         Password
@@ -100,7 +105,6 @@ function SignupFormModal() {
           required
         />
       </label>
-      {errors.password && <p className="error-message">{errors.password}</p>}
 
       <label>
         Confirm Password
@@ -111,9 +115,11 @@ function SignupFormModal() {
           required
         />
       </label>
-      {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
 
-      <button type="submit" className={email && username && password && confirmPassword ? "active" : ""}>
+      <button
+        type="submit"
+        className={email && username && password && confirmPassword ? "active" : ""}
+      >
         Sign Up
       </button>
     </form>
