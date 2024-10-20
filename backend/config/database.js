134b8caf360b3f49eef686aa12380c85b7045bbc
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-20 12:53:07 -0400

    change schema name

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,60 +8,47 @@ module.exports = {
     logQueryParameters: true,
     typeValidation: true
   },
-//   production: {
-//     use_env_variable: 'DATABASE_URL',
-//     dialect: 'postgres',
-//     seederStorage: 'sequelize',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     },
-//     define: {
-//       schema: process.env.SCHEMA
-//     }
-//   }
-// };
 
-production: {
-  use_env_variable: 'DATABASE_URL',
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    define: {
+      schema: process.env.SCHEMA
     }
-  },
-  define: {
-    schema: process.env.SCHEMA
-  }
-},
-production_url: {
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  define: {
-    schema: process.env.SCHEMA
-  }
-},
-production_url_ssl: {
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  define: {
-    schema: process.env.SCHEMA
   }
 }
-};
+//   },
+// production_url: {
+//   dialect: 'postgres',
+//   seederStorage: 'sequelize',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   define: {
+//     schema: process.env.SCHEMA
+//   }
+// },
+// production_url_ssl: {
+//   dialect: 'postgres',
+//   seederStorage: 'sequelize',
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+//   define: {
+//     schema: process.env.SCHEMA
+//   }
+// }
+// }
