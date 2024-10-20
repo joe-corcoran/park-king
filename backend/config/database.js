9ac357dfe92780b466ae1b0b760f6e8ee05c476b
Joe Corcoran <joecorcoran.com@gmail.com>
2024-10-20 13:38:52 -0400

    demo preview image

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,71 @@
+// const path = require('path');
+// const config = require('./index');
+
+// module.exports = {
+//   development: {
+//     storage: path.resolve(__dirname, 'backend/backend/dev.db'), // Update this path
+//     dialect: "sqlite",
+//     seederStorage: "sequelize",
+//     logQueryParameters: true,
+//     typeValidation: true,
+//     seedersPath: path.resolve(__dirname, '../db/seeders')
+//   },
+
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//     seedersPath: path.resolve(__dirname, '../db/seeders'),
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     },
+//     define: {
+//       schema: process.env.SCHEMA
+//     }
+//   }
+// }
+// //   },
+// // production_url: {
+// //   dialect: 'postgres',
+// //   seederStorage: 'sequelize',
+// //   dialectOptions: {
+// //     ssl: {
+// //       require: true,
+// //       rejectUnauthorized: false
+// //     }
+// //   },
+// //   define: {
+// //     schema: process.env.SCHEMA
+// //   }
+// // },
+// // production_url_ssl: {
+// //   dialect: 'postgres',
+// //   seederStorage: 'sequelize',
+// //   dialectOptions: {
+// //     ssl: {
+// //       require: true,
+// //       rejectUnauthorized: false
+// //     }
+// //   },
+// //   define: {
+// //     schema: process.env.SCHEMA
+// //   }
+// // }
+// // }
+
+
 const path = require('path');
 const config = require('./index');
 
+console.log('Current directory:', __dirname);
+console.log('Seeders path:', path.resolve(__dirname, '../db/seeders'));
+
 module.exports = {
   development: {
-    storage: path.resolve(__dirname, 'backend/backend/dev.db'), // Update this path
+    storage: path.resolve(__dirname, '../backend/dev.db'),
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
@@ -26,32 +88,4 @@ module.exports = {
       schema: process.env.SCHEMA
     }
   }
-}
-//   },
-// production_url: {
-//   dialect: 'postgres',
-//   seederStorage: 'sequelize',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   define: {
-//     schema: process.env.SCHEMA
-//   }
-// },
-// production_url_ssl: {
-//   dialect: 'postgres',
-//   seederStorage: 'sequelize',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   define: {
-//     schema: process.env.SCHEMA
-//   }
-// }
-// }
+};
